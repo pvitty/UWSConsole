6eedf9bae44629aa8f647ea47e3dcf4ba154d720
Paul Vitty <p.vitty@ulster.ac.uk>
2011-09-29 23:37:06 +0100

    Initial Rails setup

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+UWSConsole::Application.config.secret_token = '447e2fdf8bac19a56898e33d7344c915585d24437e580e7f91171750ca4f7501400b3c363c858af087d41b2e7034fd8549a88e31719732e12ef7f171019a4953'
